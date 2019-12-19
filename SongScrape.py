ac1cda669af14d84e857cdf9d43ded416462eb0a
borchr27 <30349201+borchr27@users.noreply.github.com>
2019-12-18 22:16:55 -0500

    Isolated chords for and sorted data

--- SongScrape.py
+++ SongScrape.py
@@ -22,6 +22,28 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.request
 
+
+############ FUNCTIONS #############
+
+def getChord(htmlInfo):
+    # Takes in info shown below and returns chord i.e "A_maj"
+    # '<div class="label-wrapper"><span class="chord-label label-A_maj"></span><span class="bass-label"></span></div>'
+    if len(htmlInfo) > 100 and len(htmlInfo) < 130:
+        # Finds the third instance of the dash and gives us the first coord to snip the chord from the string
+        dashArray = findOccurrences(htmlInfo, '-')
+        quoteArray = findOccurrences(htmlInfo, '"')
+        # Finds the fourt instance of quotes and gives us the last coord to snip the chord from the string
+        startSnip = dashArray[2]
+        endSnip = quoteArray[3]
+        chord = htmlInfo[startSnip+1:endSnip]
+    else:
+        chord = '  '
+    return chord
+    
+def findOccurrences(s, ch):
+    return [i for i, letter in enumerate(s) if letter == ch]
+
+
 url = f"https://chordify.net/chords/handmade-moments-where-do-you-find-the-time-official-music-video-handmade-moments"
 
 browser = webdriver.Chrome()
@@ -35,8 +57,7 @@ chords = soup.find_all(class_='label-wrapper')
 
 data = []
 for chord in chords:
-    data.append(chord)
-print(type(chord))
+    data.append(getChord(str(chord)))
 
 df = pd.DataFrame(data)
 df.to_excel('chords.xlsx')
--- findChord.py
+++ findChord.py
@@ -1,13 +1,24 @@
 text = '<div class="label-wrapper"><span class="chord-label label-A_maj"></span><span class="bass-label"></span></div>'
+text2 = 'sadmked'
 
+
+def getChord(htmlInfo):
+    # Takes in info shown below and returns chord i.e "A_maj"
+    # '<div class="label-wrapper"><span class="chord-label label-A_maj"></span><span class="bass-label"></span></div>'
+    if len(htmlInfo) > 100 and len(htmlInfo) < 130:
+        # Finds the third instance of the dash and gives us the first coord to snip the chord from the string
+        dashArray = findOccurrences(htmlInfo, '-')
+        quoteArray = findOccurrences(htmlInfo, '"')
+        # Finds the fourt instance of quotes and gives us the last coord to snip the chord from the string
+        startSnip = dashArray[2]
+        endSnip = quoteArray[3]
+        chord = htmlInfo[startSnip+1:endSnip]
+    else:
+        chord = ' test '
+    return chord
+    
 def findOccurrences(s, ch):
     return [i for i, letter in enumerate(s) if letter == ch]
 
-# Finds the third instance of the dash and gives us the first coord to snip the chord from the string
-dashArray = findOccurrences(text, '-')
-dashArray[2]
 
-# Finds the fourt instance of quotes and gives us the last coord to snip the chord from the string
-quoteArray = findOccurrences(text, '"')
-quoteArray[4]
-
+print(getChord(text))
