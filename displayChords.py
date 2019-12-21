b652428eba0498c2d997261b56edcc498e1ddc74
borchr27 <30349201+borchr27@users.noreply.github.com>
2019-12-20 20:18:58 -0500

    Small changes and planning for next time, preparing to output data to musician

--- displayChords.py
+++ displayChords.py
@@ -2,4 +2,6 @@ import pandas as pd
 excel_file = 'chords.xlsx'
 songChords = pd.read_excel(excel_file)
 
-songChords.head()
+# Add for loop here, print chords next to each other in a row, bigger font, pause for x seconds
+i = 0
+print(songChords[0][i:i+8])
