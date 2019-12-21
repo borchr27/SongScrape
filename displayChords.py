b6814c2cb3ab9204d6d3301f0e8b0f570dcdd206
borchr27 <30349201+borchr27@users.noreply.github.com>
2019-12-21 15:48:40 -0500

    Updated displayChords file working on displaying chords in terminal (done) need to work on displaying at certian rate and size

--- displayChords.py
+++ displayChords.py
@@ -1,7 +1,17 @@
 import pandas as pd
+import time
 excel_file = 'chords.xlsx'
 songChords = pd.read_excel(excel_file)
 
 # Add for loop here, print chords next to each other in a row, bigger font, pause for x seconds
+
+songLen = len(songChords[0][:])
 i = 0
-print(songChords[0][i:i+8])
+while (i < songLen):
+    j = 0
+    while j < 9:
+        print(songChords[0][i+j] + "  ", end = '')
+        j+=1
+    time.sleep(2)
+    print(" ")
+    i+=1
