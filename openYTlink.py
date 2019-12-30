aa9c471e1cc0af5197007aad1256527832b76c23
borchr27 <30349201+borchr27@users.noreply.github.com>
2019-12-29 20:17:35 -0500

    Making moves on finding getting the info I need

--- openYTlink.py
+++ openYTlink.py
@@ -6,17 +6,25 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import pandas as pd
 import requests 
+#import lxml.html
 from bs4 import BeautifulSoup
 import urllib.request
+
+# https://www.youtube.com/watch?v=5N066ISH8og&feature=emb_logo
 
 url = f"https://chordify.net/chords/rescue-by-alysha-brilla-on-ctv-live-regina-alysha-brilla"
 browser = webdriver.Chrome()
 browser.get(url)
 html = browser.page_source
 soup = BeautifulSoup(html, 'lxml')
-soup1 = soup.find_all(id_="song-page")
+soup1 = str(html.find_all('aside'))
+soup2 = soup.xpath('//div[id="data-stream"]')[0]
 
-print(soup1)
+#file2write = open("SoupData",'w')
+#file2write.write(soup1)
+#file2write.close()
+
+print(soup2)
 
 
 browser.close()
