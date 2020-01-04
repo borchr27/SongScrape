e416c3758520e904a83fee0a558490ca3e52bc8b
borchr27 <30349201+borchr27@users.noreply.github.com>
2020-01-04 08:12:05 -0500

    Minor changes documenting notes and tutorials used

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ def main():
 if __name__ == '__main__':
     main()
 
+def notes():
+    # A funtion merely to hold and hide some of the notes and tuts that were used to build this program
+    pass
+    # Helpful tuts for structuring program:
+    # Classes: https://www.youtube.com/watch?v=m6hhUBXcA-E
+    # Classes Cont: https://www.youtube.com/watch?v=apACNr7DC_s
+    # Init / Name Method: https://www.youtube.com/watch?v=WIP3-woodlU
