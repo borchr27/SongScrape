b19f15ba96fc78bc37fd2211acb71866a6f23e25
borchr27 <30349201+borchr27@users.noreply.github.com>
2020-01-27 18:36:49 -0500

    Working on navigating to proper page in Ultimate Guitar still

--- UltimateGuitar.py
+++ UltimateGuitar.py
@@ -18,34 +18,45 @@ class WebsiteScraper:
         self.soup = None
 
     def website_scraper(self):
-        # First block gets chord data, second block gets song youtube link
+        # Jimmy Buffet Margaritaville
+        song_info = input("Enter Artist Name and Song Name: ")
+        # Modifies entered text to be concatenated to URL
+        song_info.replace(' ', '%20')
         driver = webdriver.Chrome()
-        driver.get("https://www.ultimate-guitar.com/")
-        #driver.get('https://www.ultimate-guitar.com/search.php?search_type=title&value=STRING%20STRING')
-        # Finds the search input box
-        search = driver.find_element_by_name("value")
-        search.clear()
-        search.send_keys('Jimmy Buffett Margaritaville')
-        search.send_keys(Keys.RETURN)
-        time.sleep(5)
+        # Uses user entered search terms to search Ultimate Guitar
+        driver.get('https://www.ultimate-guitar.com/search.php?search_type=title&value={}'.format(song_info))
+        # Create the soup to search for items
         html = driver.page_source
         self.soup = BeautifulSoup(html, 'lxml')
         # The class name here is used to indicate the song name
         options = self.soup.find_all(class_='pZcWD')
-        #options = driver.find_element_by_class_name('pZcWD')
         # The class name here is used to tell how many users have ranked the song
         rankings = self.soup.find_all(class_='_31dWM')
         # The class name here is used to tell the rating of the song
 
+        choices = df = pd.DataFrame(columns=["Rating","Description","Link"])
+        for description, ranking, link in zip(options[3:7], rankings[1:5], options[3:7]):
+            link = link.find('a')
+            choices = df.append(pd.Series([ranking.get_text(), description.get_text(), link.get('href')], index=df.columns ), ignore_index=True)
+  
+
+            #df = pd.DataFrame({"Rating": [ranking.get_text()], "Description": [description.get_text()], "Link": [link.get('href')]})
+            #choices.append(df, ignore_index=True)
         
-        for option in options[3:7]:
-            print(option.get_text())
+        for row in choices:
+            print(row.loc[0,:])
+            
+        """
+        for description in options[3:7]:
+            print(description.get_text())
+
         for ranking in rankings[1:5]:
             print(ranking.get_text())
-        options = options.find_all('a')
-        for a in options[3:7]:
-            print(a.get('href')) 
-        """
+
+        for link in options[3:7]:
+            link = link.find('a')
+            print(link.get('href')) 
+        
         for item in options:
             self.soup.find_all('a')
             print(link.get('href'))
